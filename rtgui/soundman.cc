3a5b6729db3ba8701505fc34a8481456f64830bd
aferrero2707 <aferrero1975@gmail.com>
2017-10-20 11:33:24 +0200

    Temporarly disabled camberra-gtk code

--- rtgui/soundman.cc
+++ rtgui/soundman.cc
@@ -26,9 +26,9 @@
 #include <mmsystem.h>
 #endif
 
-#ifdef __linux__
-#include <canberra-gtk.h>
-#endif
+//#ifdef __linux__
+//#include <canberra-gtk.h>
+//#endif
 
 
 void SoundManager::init()
@@ -67,7 +67,7 @@ void SoundManager::playSoundAsync(const Glib::ustring &sound)
     wchar_t *wfilename = (wchar_t*)g_utf8_to_utf16 (sound.c_str(), -1, NULL, NULL, NULL);
     PlaySoundW(wfilename, NULL, sndParam);
     g_free( wfilename );
-#elif defined(__linux__)
-    ca_context_play(ca_gtk_context_get(), 0, CA_PROP_EVENT_ID, sound.c_str(), CA_PROP_MEDIA_FILENAME, sound.c_str(), NULL);
+//#elif defined(__linux__)
+//    ca_context_play(ca_gtk_context_get(), 0, CA_PROP_EVENT_ID, sound.c_str(), CA_PROP_MEDIA_FILENAME, sound.c_str(), NULL);
 #endif
 }
