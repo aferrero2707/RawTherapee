e28890df4843d66a220d59ca2c7a5b7e93fd909c
aferrero2707 <aferrero1975@gmail.com>
2017-11-20 12:16:54 +0100

    Updated Win64 CI configuration

--- ci/build-w64.sh
+++ ci/build-w64.sh
@@ -3,11 +3,11 @@
 echo "Compiling RawTherapee"
 ls /sources
 (crossroad cmake -DCMAKE_BUILD_TYPE=Release -DPROC_TARGET_NUMBER=1 \
- -DCACHE_NAME_SUFFIX='"5-dev"' \
- -DCMAKE_C_FLAGS='"-mwin32 -m64 -mthreads -msse2"' \
- -DCMAKE_C_FLAGS_RELEASE='"-DNDEBUG -O2"' \
- -DCMAKE_CXX_FLAGS='"-mwin32 -m64 -mthreads -msse2"' \
- -DCMAKE_CXX_FLAGS_RELEASE='"-Wno-aggressive-loop-optimizations -DNDEBUG -O3"' \
- -DCMAKE_EXE_LINKER_FLAGS '"-m64 -mthreads -static-libgcc"' \
- -DCMAKE_EXE_LINKER_FLAGS_RELEASE='"-s -O3"' \
+ -DCACHE_NAME_SUFFIX="'5-dev'" \
+ -DCMAKE_C_FLAGS="'-mwin32 -m64 -mthreads -msse2'" \
+ -DCMAKE_C_FLAGS_RELEASE="'-DNDEBUG -O2'" \
+ -DCMAKE_CXX_FLAGS="'-mwin32 -m64 -mthreads -msse2'" \
+ -DCMAKE_CXX_FLAGS_RELEASE="'-Wno-aggressive-loop-optimizations -DNDEBUG -O3'" \
+ -DCMAKE_EXE_LINKER_FLAGS="'-m64 -mthreads -static-libgcc'" \
+ -DCMAKE_EXE_LINKER_FLAGS_RELEASE="'-s -O3'" \
  /sources && make -j 2 && make install) || exit 1
